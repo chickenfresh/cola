e7a061fb74c63b074b7aa7979e54d9a11cf21bf1
Nikita Alekseevich <arjuno23192122@gmail.com>
2018-12-06 20:08:13 +0300

    better version

--- cola.py
+++ cola.py
@@ -1,21 +1,13 @@
 # cola.py
-def calc_bottles(budg, price) -> (None, int):
-    result = budg // price
-    return None if result == 0 else result
-
-
-def get_min_price(budget=0, price=7, k=3) -> int:
-    starting_budget = budget
+def get_min_price(price=7, k=3):
+    today_money = price
     for i in range(4):
-        bottles = calc_bottles(budget, price)
-        if not bottles:
-            return get_min_price(starting_budget + 1, price, k)
-        budget = budget % price + bottles * k
-    bottles = calc_bottles(budget, price)
-    if not bottles:
-        return get_min_price(starting_budget + 1, price, k)
-    budget = budget % price
-    return starting_budget if budget == 0 and bottles == 1 else get_min_price(starting_budget + 1, price, k)
+        bottles = (today_money - price + 1) // k
+        if (today_money - price + 1) % k != 0:
+            bottles += 1
+        c = today_money - bottles * k
+        today_money = bottles * price + c
+    return today_money
 
 
 print(get_min_price())
